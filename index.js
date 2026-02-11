7fbccc0e2a3a8ac983cb61895b9b9d1035e085ad
Shoyon Kumar <shoyonkumar143@gmail.com>
2026-02-11 16:55:18 +0600

    active button functionality implemented

--- index.js
+++ index.js
@@ -4,11 +4,22 @@ const loadLessons = () => {
     .then((json) => displayLessons(json?.data));
 };
 
+const removeActive = () => {
+  const lessonButtons = document.querySelectorAll(".lesson-btn");
+  lessonButtons.forEach((btn) => btn.classList.remove("active"));
+};
+
 const loadLevelWord = (id) => {
   const url = `https://openapi.programming-hero.com/api/level/${id}`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => displayLevelWord(data?.data));
+    .then((data) => {
+      removeActive(); //remove all active class
+      const clickedBtn = document.getElementById(`lesson-btn-${id}`);
+      // console.log(clickedBtn);
+      clickedBtn.classList.add("active"); // add active class
+      displayLevelWord(data?.data);
+    });
 };
 
 const displayLevelWord = (words) => {
@@ -59,7 +70,7 @@ const displayLessons = (lessons) => {
     // 3. create element
     const btnDiv = document.createElement("div");
     btnDiv.innerHTML = `
-        <button onclick="loadLevelWord(${lesson?.level_no})" class="btn btn-outline btn-primary">
+        <button id="lesson-btn-${lesson.level_no}" onclick="loadLevelWord(${lesson?.level_no})" class="btn btn-outline btn-primary lesson-btn">
         <i class="fa-solid fa-book-open"></i>Lesson - ${lesson?.level_no}
         </button>
     `;
