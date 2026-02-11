0569e4fd0d5183a5fae9db43200a4faf255ed58e
Shoyon Kumar <shoyonkumar143@gmail.com>
2026-02-11 14:52:55 +0600

    heading section banner section and dynamic  vocabulaty section created

---
+++ index.js
@@ -0,0 +1,59 @@
+const loadLessons = () => {
+  fetch("https://openapi.programming-hero.com/api/levels/all")
+    .then((res) => res.json())
+    .then((json) => displayLessons(json?.data));
+};
+
+const loadLevelWord = (id) => {
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayLevelWord(data?.data));
+};
+
+const displayLevelWord = (words) => {
+  const wordContainer = document.getElementById("word-container");
+  wordContainer.innerHTML = "";
+  for (let word of words) {
+    const card = document.createElement("div");
+    card.innerHTML = `
+    <div
+        class="bg-white rounded-lg shadow-sm text-center py-10 px-5 space-y-4"
+      >
+        <h2 class="font-semibold text-2xl">${word.word}</h2>
+        <p class="font-medium">Meaning /Pronounciation</p>
+        <div class="font-bangla text-xl font-semibold text-gray-600">
+          "${word.meaning} / ${word.pronunciation}"
+        </div>
+        <div class="flex justify-between items-center">
+          <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
+            <i class="fa-solid fa-circle-info"></i>
+          </button>
+          <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
+            <i class="fa-solid fa-volume-high"></i>
+          </button>
+        </div>
+      </div>
+`;
+    wordContainer.append(card);
+  }
+};
+
+const displayLessons = (lessons) => {
+  // 1. get the container
+  const levelContainer = document.getElementById("level-container");
+  levelContainer.innerHTML = "";
+  // 2. get into every lessons
+  for (let lesson of lessons) {
+    // 3. create element
+    const btnDiv = document.createElement("div");
+    btnDiv.innerHTML = `
+        <button onclick="loadLevelWord(${lesson?.level_no})" class="btn btn-outline btn-primary">
+        <i class="fa-solid fa-book-open"></i>Lesson - ${lesson?.level_no}
+        </button>
+    `;
+    // 4. append into container
+    levelContainer.append(btnDiv);
+  }
+};
+loadLessons();
