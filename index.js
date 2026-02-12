2706e265068045f640b5929ea0cb5322baac47b6
Shoyon Kumar <shoyonkumar143@gmail.com>
2026-02-12 18:03:48 +0600

    pronouncieation of  word functionality implemented

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const creatElement = (arr) => {
   return htmlElements.join(" ");
 };
 
+function pronounceWord(word) {
+  const utterance = new SpeechSynthesisUtterance(word);
+  utterance.lang = "en-EN"; // English
+  window.speechSynthesis.speak(utterance);
+}
+
 const manageSpinner = (status) => {
   if (status == true) {
     document.getElementById("spinner").classList.remove("hidden");
@@ -99,7 +105,7 @@ const displayLevelWord = (words) => {
           <button onclick="loadWordDetail(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
             <i class="fa-solid fa-circle-info"></i>
           </button>
-          <button  class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
+          <button onclick="pronounceWord('${word.word}')"  class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
             <i class="fa-solid fa-volume-high"></i>
           </button>
         </div>
